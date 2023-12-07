3f43c47343c8c246d90d8d806cc9905f35e25486
Guilherme Martins <guilherme.mleal.11@gmail.com>
2023-12-06 21:43:47 -0300

    feat: auto instancied fan
    Including in serializer auto intance

--- codigo_projeto/back-end/Fan/serializer/TemperatureSerializer.py
+++ codigo_projeto/back-end/Fan/serializer/TemperatureSerializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from Fan.models.TemperatureModel import TemparatureEntity
+from Fan.models.FanModel import FanEntity
 
 class TemperatureEntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = TemparatureEntity
         fields = '__all__'
+
+    def create(self, validated_data):
+        temperature_entity = TemparatureEntity.objects.create(**validated_data)
+        temperature = validated_data['temperature']
+
+        if temperature >= 28.00:
+            fan = FanEntity(turnOn=True)  # Cria uma instância de FanEntity
+            fan.save()  # Salva a instância de FanEntity no banco de dados
+
+        return temperature_entity
